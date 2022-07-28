2add7d600b1b0e7dc38d40932bb80a17e5b013ee
Senad Shabaj <senadshabaj73@gmail.com>
2022-07-28 15:31:28 +0200

    fix

--- classes/handlers/directory_handler.py
+++ classes/handlers/directory_handler.py
@@ -5,9 +5,16 @@ from datetime import date
 class DirectoryHandler():
 
     def mkdir_today(self, path):
+        main_dir = os.getcwd()
+        dir_list =main_dir.split('\\')
+        documents_dir = f'{dir_list[0]}/{dir_list[1]}/{dir_list[2]}/Documents/{path}'
         today = date.today()
         dir_name = today.strftime("%d-%m-%Y")
-        if_exitsts = os.path.exists(f'{path}/{dir_name}')
+        if_exitsts_logs = os.path.exists(f'{dir_list[0]}/{dir_list[1]}/{dir_list[2]}/Documents/{path}')
+        if_exitsts = os.path.exists(f'{dir_list[0]}/{dir_list[1]}/{dir_list[2]}/Documents/{path}/{dir_name}')
+
+        if not if_exitsts_logs:
+            os.mkdir(f'{dir_list[0]}/{dir_list[1]}/{dir_list[2]}/Documents/{path}')
 
         if not if_exitsts:
-            os.mkdir(f'{path}/{dir_name}')
+            os.mkdir(f'{dir_list[0]}/{dir_list[1]}/{dir_list[2]}/Documents/{path}/{dir_name}')
